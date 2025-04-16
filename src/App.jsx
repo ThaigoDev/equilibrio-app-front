e1106c2cd46cd752ff46281b9946cee9c2a741ea
ThaigoDev <thaigodeveloper@gmail.com>
2025-04-16 09:14:27 -0300

    att

---
+++ src/App.jsx
@@ -0,0 +1,464 @@
+import "./App.css"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="main">
+        <img className="image-2" src="image-20.png" />
+        <img
+          className="_4-df-24-bb-8-fee-2-4-acf-8-ce-6-f-70860309314-1"
+          src="_4-df-24-bb-8-fee-2-4-acf-8-ce-6-f-70860309314-10.png"
+        />
+        <div className="building-blocks-leading-element">
+          <svg
+            className="icon"
+            width="26"
+            height="25"
+            viewBox="0 0 26 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.0209 22.9166L9.58755 19.5833C9.35283 19.4965 9.13165 19.3923 8.92401 19.2708C8.71637 19.1493 8.51324 19.0191 8.31463 18.8802L5.09172 20.1823L2.11255 15.2344L4.90213 13.2031C4.88408 13.0816 4.87505 12.9644 4.87505 12.8515V12.1484C4.87505 12.0356 4.88408 11.9184 4.90213 11.7969L2.11255 9.7656L5.09172 4.81769L8.31463 6.11977C8.51324 5.98088 8.72088 5.85067 8.93755 5.72915C9.15422 5.60762 9.37088 5.50345 9.58755 5.41665L10.0209 2.08331H15.9792L16.4125 5.41665C16.6473 5.50345 16.8685 5.60762 17.0761 5.72915C17.2837 5.85067 17.4869 5.98088 17.6855 6.11977L20.9084 4.81769L23.8876 9.7656L21.098 11.7969C21.116 11.9184 21.1251 12.0356 21.1251 12.1484V12.8515C21.1251 12.9644 21.107 13.0816 21.0709 13.2031L23.8605 15.2344L20.8813 20.1823L17.6855 18.8802C17.4869 19.0191 17.2792 19.1493 17.0625 19.2708C16.8459 19.3923 16.6292 19.4965 16.4125 19.5833L15.9792 22.9166H10.0209ZM13.0542 16.1458C14.1014 16.1458 14.9952 15.7899 15.7355 15.0781C16.4757 14.3663 16.8459 13.5069 16.8459 12.5C16.8459 11.493 16.4757 10.6337 15.7355 9.92185C14.9952 9.21005 14.1014 8.85415 13.0542 8.85415C11.9889 8.85415 11.0907 9.21005 10.3594 9.92185C9.62817 10.6337 9.26255 11.493 9.26255 12.5C9.26255 13.5069 9.62817 14.3663 10.3594 15.0781C11.0907 15.7899 11.9889 16.1458 13.0542 16.1458Z"
+              fill="black"
+            />
+          </svg>
+        </div>
+        <div className="line-1"></div>
+        <div className="como-voc-est-se-sentindo">Como você está se sentindo?</div>
+        <div className="h-bitos-saud-veis">Hábitos saudáveis</div>
+        <div className="ofensiva">Ofensiva</div>
+        <div className="calend-rio-emocional">Calendário emocional</div>
+        <div className="seu-progresso">Seu Progresso</div>
+        <img className="image-1" src="image-10.png" />
+        <div className="plain-tooltip">
+          <div className="supporting-text">Deixe uma nota...</div>
+        </div>
+        <div className="button">
+          <div className="state-layer">
+            <div className="label-text">Enviar</div>
+          </div>
+        </div>
+        <div className="line-2"></div>
+        <div className="line-4"></div>
+        <img
+          className="bd-2270-f-0-54-ab-4272-883-c-481-d-9-b-3-b-6-bda-1"
+          src="bd-2270-f-0-54-ab-4272-883-c-481-d-9-b-3-b-6-bda-10.png"
+        />
+        <div className="gua">Água</div>
+        <div className="exerc-cio">Exercício</div>
+        <div className="sono">Sono</div>
+        <div className="_00-min">00 min</div>
+        <div className="_4-7-dias">
+          <span>
+            <span className="_4-7-dias-span">4</span>
+            <span className="_4-7-dias-span2">/7 dias</span>
+          </span>
+        </div>
+        <div className="_00-min2">00 min</div>
+        <div className="_0-8-copos">0 / 8 copos</div>
+        <div className="rectangle-2"></div>
+        <div className="line-5"></div>
+        <div className="line-6"></div>
+        <div className="line-7"></div>
+        <img
+          className="bd-2270-f-0-54-ab-4272-883-c-481-d-9-b-3-b-6-bda-2"
+          src="bd-2270-f-0-54-ab-4272-883-c-481-d-9-b-3-b-6-bda-20.png"
+        />
+        <div className="gua">Água</div>
+        <div className="jan-2025">Jan,2025</div>
+        <div className="exerc-cio">Exercício</div>
+        <div className="sono2">Sono</div>
+        <div className="peso">Peso</div>
+        <div className="_00-60-min">00 / 60 min</div>
+        <div className="_00-60-min2">00 / 60 min</div>
+        <div className="_00-60-min2">00 / 60 min</div>
+        <div className="_70-5-60-5-kg">70,5 / 60,5 kg</div>
+        <div className="_0-8-copos">0 / 8 copos</div>
+        <img
+          className="b-297-afa-3-67-a-5-4-dee-85-bb-8-b-20-aaf-724-ac-1"
+          src="b-297-afa-3-67-a-5-4-dee-85-bb-8-b-20-aaf-724-ac-10.png"
+        />
+        <div className="modal-date-picker">
+          <div className="local-calendar-grid">
+            <div className="days-of-the-week">
+              <div className="sunday">
+                <div className="date">S</div>
+              </div>
+              <div className="monday">
+                <div className="date">M</div>
+              </div>
+              <div className="tuesday">
+                <div className="date">T</div>
+              </div>
+              <div className="wednesday">
+                <div className="date">W</div>
+              </div>
+              <div className="thursday">
+                <div className="date">T</div>
+              </div>
+              <div className="friday">
+                <div className="date">F</div>
+              </div>
+              <div className="saturday">
+                <div className="date">S</div>
+              </div>
+            </div>
+            <div className="week-1">
+              <div className="sunday2">
+                <div className="container">
+                  <div className="state-layer2">
+                    <div className="empty-container"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="monday2">
+                <div className="container">
+                  <div className="state-layer2">
+                    <div className="empty-container"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="tuesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">1</div>
+                  </div>
+                </div>
+              </div>
+              <div className="wednesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">2</div>
+                  </div>
+                </div>
+              </div>
+              <div className="thursday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">3</div>
+                  </div>
+                </div>
+              </div>
+              <div className="friday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">4</div>
+                  </div>
+                </div>
+              </div>
+              <div className="saturday">
+                <div className="container3">
+                  <div className="state-layer2">
+                    <div className="date2">5</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="week-2">
+              <div className="sunday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">6</div>
+                  </div>
+                </div>
+              </div>
+              <div className="monday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">7</div>
+                  </div>
+                </div>
+              </div>
+              <div className="tuesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">8</div>
+                  </div>
+                </div>
+              </div>
+              <div className="wednesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">9</div>
+                  </div>
+                </div>
+              </div>
+              <div className="thursday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">10</div>
+                  </div>
+                </div>
+              </div>
+              <div className="friday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">11</div>
+                  </div>
+                </div>
+              </div>
+              <div className="saturday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">12</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="week-3">
+              <div className="sunday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">13</div>
+                  </div>
+                </div>
+              </div>
+              <div className="monday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">14</div>
+                  </div>
+                </div>
+              </div>
+              <div className="tuesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">15</div>
+                  </div>
+                </div>
+              </div>
+              <div className="wednesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">16</div>
+                  </div>
+                </div>
+              </div>
+              <div className="thursday2">
+                <div className="container4">
+                  <div className="state-layer3">
+                    <div className="date3">17</div>
+                  </div>
+                </div>
+              </div>
+              <div className="friday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">18</div>
+                  </div>
+                </div>
+              </div>
+              <div className="saturday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">19</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="week-4">
+              <div className="sunday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">20</div>
+                  </div>
+                </div>
+              </div>
+              <div className="monday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">21</div>
+                  </div>
+                </div>
+              </div>
+              <div className="tuesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">22</div>
+                  </div>
+                </div>
+              </div>
+              <div className="wednesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">23</div>
+                  </div>
+                </div>
+              </div>
+              <div className="thursday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">24</div>
+                  </div>
+                </div>
+              </div>
+              <div className="friday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">25</div>
+                  </div>
+                </div>
+              </div>
+              <div className="saturday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">26</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="week-5">
+              <div className="sunday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">27</div>
+                  </div>
+                </div>
+              </div>
+              <div className="monday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">28</div>
+                  </div>
+                </div>
+              </div>
+              <div className="tuesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">29</div>
+                  </div>
+                </div>
+              </div>
+              <div className="wednesday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">30</div>
+                  </div>
+                </div>
+              </div>
+              <div className="thursday">
+                <div className="container2">
+                  <div className="state-layer3">
+                    <div className="date">31</div>
+                  </div>
+                </div>
+              </div>
+              <div className="friday2">
+                <div className="container">
+                  <div className="state-layer2">
+                    <div className="empty-container"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="saturday2">
+                <div className="container">
+                  <div className="state-layer2">
+                    <div className="empty-container"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="linear-determinate-progress-indicator">
+          <div className="active-indicator">
+            <svg
+              className="segment-start"
+              width="40"
+              height="12"
+              viewBox="0 0 40 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M40 6L30 6L10 6L0 6"
+                stroke="#FF9500"
+                stroke-width="4"
+                stroke-linecap="round"
+              />
+            </svg>
+            <svg
+              className="segment"
+              width="80"
+              height="12"
+              viewBox="0 0 80 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0 6H20H60H80"
+                stroke="#FF9500"
+                stroke-width="4"
+                stroke-linecap="round"
+              />
+            </svg>
+            <svg
+              className="segment-end"
+              width="41"
+              height="12"
+              viewBox="0 0 41 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.841553 6H10.8415H30.8415H40.8416"
+                stroke="#FF9500"
+                stroke-width="4"
+                stroke-linecap="round"
+              />
+            </svg>
+          </div>
+          <div className="track-and-stop">
+            <div className="track">
+              <div className="track2"></div>
+            </div>
+            <div className="stop">
+              <div className="stop-shape"></div>
+            </div>
+          </div>
+        </div>
+        <img className="tl-1" src="tl-10.png" />
+        <img
+          className="_67-f-6-f-760-9048-4-d-43-95-cd-534-d-1475187-a-1"
+          src="_67-f-6-f-760-9048-4-d-43-95-cd-534-d-1475187-a-10.png"
+        />
+        <svg
+          className="navigate-before"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M14 18L8 12L14 6L15.4 7.4L10.8 12L15.4 16.6L14 18Z"
+            fill="#1D1B20"
+          />
+        </svg>
+        <svg
+          className="navigate-next"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.6 12L8 7.4L9.4 6L15.4 12L9.4 18L8 16.6L12.6 12Z"
+            fill="#1D1B20"
+          />
+        </svg>
+        <img
+          className="_3-fea-4547-8857-4415-a-83-a-c-56668-b-8-e-854-1"
+          src="_3-fea-4547-8857-4415-a-83-a-c-56668-b-8-e-854-10.png"
+        />
+        <div className="peso">Peso</div>
+        <div className="_70-5-60-5-kg">70,5 / 60,5 kg</div>
+      </div>
+    </>
+  )
+}
+export default App
