8b90dfa8efb9e02cd1395d54219c1d556de4be34
Cassio024 <cassioflhblack26@gmail.com>
2025-04-20 10:56:54 -0300

    Atualização do layout: frase 'Como está se sentindo?

--- src/pages/Sentimentos.jsx
+++ src/pages/Sentimentos.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import "./Sentimentos.css";
 
+// Importando as imagens dos emojis
+import emojiMuitoFeliz from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.23_9c9f2168.jpg";
+import emojiFeliz from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.24_4326a1f3.jpg";
+import emojiNeutro from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.25_1d6230cf.jpg";
+import emojiTriste from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.25_c745f949.jpg";
+import emojiMuitoTriste from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.27_81646dac.jpg";
+
 const Sentimentos = () => {
   return (
     <div className="sentimentos-container">
       <h2 className="titulo">Como você está se sentindo?</h2>
+      <p className="subtitulo">Como Você está se sentindo?</p>
       <div className="emojis">
-        <img className="emoji" src="" alt="Muito feliz" />
-        <img className="emoji" src="/emojis/emoji-verde-claro.png" alt="Feliz" />
-        <img className="emoji" src="/emojis/emoji-amarelo.png" alt="Neutro" />
-        <img className="emoji" src="/emojis/emoji-laranja.png" alt="Triste" />
-        <img className="emoji" src="/emojis/emoji-vermelho.png" alt="Muito triste" />
+        <img className="emoji" src={emojiMuitoFeliz} alt="Muito feliz" />
+        <img className="emoji" src={emojiFeliz} alt="Feliz" />
+        <img className="emoji" src={emojiNeutro} alt="Neutro" />
+        <img className="emoji" src={emojiTriste} alt="Triste" />
+        <img className="emoji" src={emojiMuitoTriste} alt="Muito triste" />
       </div>
       <div className="note-wrapper">
         <div className="note-container">
-          <input 
-            type="text" 
-            className="nota-input" 
-            placeholder="Deixe uma nota..." 
+          <input
+            type="text"
+            className="nota-input"
+            placeholder="Deixe uma nota..."
           />
         </div>
         <button className="enviar-button">Enviar</button>
