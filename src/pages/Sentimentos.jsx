69730fa6eefb80d433defc3e37210875f08991d0
Cassio024 <cassioflhblack26@gmail.com>
2025-04-21 13:38:31 -0300

    atualização images

--- src/pages/Sentimentos.jsx
+++ src/pages/Sentimentos.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Sentimentos.css";
 
-// Importando as imagens dos emojis
-import emojiMuitoFeliz from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.23_9c9f2168.jpg";
-import emojiFeliz from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.24_4326a1f3.jpg";
-import emojiNeutro from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.25_1d6230cf.jpg";
-import emojiTriste from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.25_c745f949.jpg";
-import emojiMuitoTriste from "../images/Imagem do WhatsApp de 2025-04-18 à(s) 14.22.27_81646dac.jpg";
+// Importando as imagens dos emojis com caminhos relativos
+import emojiMuitoFeliz from "../images/oie_transparent (13).png";
+import emojiFeliz from "../images/oie_transparent (14).png";
+import emojiNeutro from "../images/oie_transparent (15).png";
+import emojiTriste from "../images/oie_transparent (16).png";
+import emojiMuitoTriste from "../images/oie_transparent (17).png";
 
 const Sentimentos = () => {
   return (
     <div className="sentimentos-container">
       <h2 className="titulo">Como você está se sentindo?</h2>
-      <p className="subtitulo">Como Você está se sentindo?</p>
+      <p className="subtitulo">Como você está se sentindo?</p>
       <div className="emojis">
         <img className="emoji" src={emojiMuitoFeliz} alt="Muito feliz" />
         <img className="emoji" src={emojiFeliz} alt="Feliz" />
