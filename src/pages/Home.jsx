e865a94bd51323310ddd0a49babb75243d49cb3e
Cassio024 <cassioflhblack26@gmail.com>
2025-04-21 13:18:33 -0300

    importação senttings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Equilibrio from "./Equilibrio"; // Ajuste o caminho caso necessário
 import Sentimentos from "./Sentimentos"; // Certifique-se de ajustar o caminho
+import Settings from "./Settings"; // Importando o novo componente Settings
 import "./Home.css";
 
 const Home = () => {
     return (
         <div className="home-container">
+            {/* Componentes principais */}
             <Equilibrio />
             <Sentimentos />
+            
+            {/* Novo componente Settings */}
+            <Settings />
         </div>
     );
 };
