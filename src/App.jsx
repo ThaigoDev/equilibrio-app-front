9cd9844bf33a2b60bfd0e9287c85807fc2f9c348
ThaigoDev <thaigodeveloper@gmail.com>
2025-04-23 20:17:13 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/settings" element={<Settings />} />
         
       </Routes>
     </Router>
