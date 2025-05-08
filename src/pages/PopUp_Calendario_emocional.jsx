b3d865a199b24c48e58924ae396ce26174b7e65d
Ygormarques7 <ygormarques12k@gmail.com>
2025-05-08 12:02:52 -0300

    Criei os arquivos e rotas

---
+++ src/pages/PopUp_Calendario_emocional.jsx
@@ -0,0 +1,16 @@
+import "./PopUp_Calendario_emocional.css"
+
+
+function PopUp_Calendario_emocional () {
+
+    return (
+        <div>
+            <h2> PopUp Calendario</h2>
+            {                 }
+
+        </div>
+
+    );
+}
+
+export default PopUp_Calendario_emocional
---
+++ src/pages/PopUp_habitos_saudaveis.jsx
@@ -0,0 +1,18 @@
+import "./PopUp_habitos_saudaveis"
+
+
+function PopUp_habitos_saudaveis() {
+
+
+    return (
+        <div>
+            <h2> PopUp Habitos Saudaveis</h2>
+            {                 }
+
+        </div>
+
+    );
+
+}
+
+export default PopUp_habitos_saudaveis
