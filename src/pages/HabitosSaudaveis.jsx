fdddc005fe3aee733c7228e68af44bc6cabf217c
Timoteo Augusto <timoteoasteixeira@gmail.com>
2025-05-25 15:42:52 -0300

    retirado o fundo preto das figuras de Habitos Saudaveis

--- src/pages/HabitosSaudaveis.jsx
+++ src/pages/HabitosSaudaveis.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './HabitosSaudaveis.css';
 
+import aguaIcon from '../images/agua.png';
+import exercicioIcon from '../images/exercicio.png';
+import sonoIcon from '../images/sono.png';
+import pesoIcon from '../images/peso.png';
+
 const HabitosSaudaveis = () => {
   // Dados mockados - ESTES DADOS SERÃO SUBSTITUÍDOS PELO BACKEND
   const habitos = {
@@ -11,52 +16,65 @@ const HabitosSaudaveis = () => {
   };
 
   return (
-    <div className="habitos-container">
-      <h2 className="habitos-titulo">Hábitos saudáveis</h2>
-      
-      <div className="habitos-box">
-        {/* ÁGUA - Tags para conexão com backend */}
-        <div className="habito-item">
+  <div className="habitos-container">
+    <h2 className="habitos-titulo">Hábitos saudáveis</h2>
+    
+    <div className="habitos-box">
+      {/* ÁGUA */}
+      <div className="habito-item">
+        <div className="habito-info">
+          <img src={aguaIcon} alt="Ícone água" className="habito-icon" />
           <span className="habito-nome">Água</span>
-          <span className="habito-valor" 
-                data-agua-atual={habitos.agua.atual} 
-                data-agua-meta={habitos.agua.meta}>
-            {habitos.agua.atual} / {habitos.agua.meta} copos
-          </span>
         </div>
-        
-        {/* EXERCÍCIO - Tags para conexão com backend */}
-        <div className="habito-item">
+        <span className="habito-valor" 
+              data-agua-atual={habitos.agua.atual} 
+              data-agua-meta={habitos.agua.meta}>
+          {habitos.agua.atual} / {habitos.agua.meta} copos
+        </span>
+      </div>
+
+      {/* EXERCÍCIO */}
+      <div className="habito-item">
+        <div className="habito-info">
+          <img src={exercicioIcon} alt="Ícone exercício" className="habito-icon" />
           <span className="habito-nome">Exercício</span>
-          <span className="habito-valor" 
-                data-exercicio-atual={habitos.exercicio.atual} 
-                data-exercicio-meta={habitos.exercicio.meta}>
-            {habitos.exercicio.atual.toString().padStart(2, '0')} / {habitos.exercicio.meta} min
-          </span>
         </div>
-        
-        {/* SONO - Tags para conexão com backend */}
-        <div className="habito-item">
+        <span className="habito-valor" 
+              data-exercicio-atual={habitos.exercicio.atual} 
+              data-exercicio-meta={habitos.exercicio.meta}>
+          {habitos.exercicio.atual.toString().padStart(2, '0')} / {habitos.exercicio.meta} min
+        </span>
+      </div>
+
+      {/* SONO */}
+      <div className="habito-item">
+        <div className="habito-info">
+          <img src={sonoIcon} alt="Ícone sono" className="habito-icon" />
           <span className="habito-nome">Sono</span>
-          <span className="habito-valor" 
-                data-sono-atual={habitos.sono.atual} 
-                data-sono-meta={habitos.sono.meta}>
-            {habitos.sono.atual.toString().padStart(2, '0')} / {habitos.sono.meta} min
-          </span>
         </div>
-        
-        {/* PESO - Tags para conexão com backend */}
-        <div className="habito-item">
+        <span className="habito-valor" 
+              data-sono-atual={habitos.sono.atual} 
+              data-sono-meta={habitos.sono.meta}>
+          {habitos.sono.atual.toString().padStart(2, '0')} / {habitos.sono.meta} min
+        </span>
+      </div>
+
+      {/* PESO */}
+      <div className="habito-item">
+        <div className="habito-info">
+          <img src={pesoIcon} alt="Ícone peso" className="habito-icon" />
           <span className="habito-nome">Peso</span>
-          <span className="habito-valor" 
-                data-peso-atual={habitos.peso.atual} 
-                data-peso-meta={habitos.peso.meta}>
-            {habitos.peso.atual} / {habitos.peso.meta} kg
-          </span>
         </div>
+        <span className="habito-valor" 
+              data-peso-atual={habitos.peso.atual} 
+              data-peso-meta={habitos.peso.meta}>
+          {habitos.peso.atual} / {habitos.peso.meta} kg
+        </span>
       </div>
     </div>
-  );
+  </div>
+);
+
 };
 
 export default HabitosSaudaveis;
